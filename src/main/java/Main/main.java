b263a43968bbf30d364ad4375e425104754c38cf
Issa-hub14 <isabellaquijano1@gmail.com>
2026-04-11 21:35:36 -0500

    Se crea la clase main

---
+++ src/main/java/Main/main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+/**
+ *
+ * @author isabe
+ */
+import Controlador.RectanguloControlador;
+import Vista.VistaRectangulo;
+public class main {
+    
+    public static void main(String[] args) {
+        RectanguloControlador controlador = new RectanguloControlador(100);
+        new VistaRectangulo(controlador).setVisible(true);
+    
+    }
+}
